94e1009eee147cf1073ff4cb177a473d06c180ee
Juan Coutinho <desenvolvedor.juan.business@gmail.com>
2025-05-25 09:58:20 -0300

    Create exercise-1.rb

---
+++ exercise-1.rb
@@ -0,0 +1,11 @@
+# Soma simples de dois números com os valores informados pelo usuário
+
+p "Write the first number:"
+n1 = gets.chomp.to_i 
+
+p "Write the second number:"
+n2 = gets.chomp.to_i 
+
+result = n1 + n2 
+
+p "The result is #{result}"
